b29a40ea7a939b8c39016c96ff10b2480c959c9b
Luke Marsden <me@lukemarsden.net>
2016-09-22 12:09:52 +0100

    better logs

--- run.sh
+++ run.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-set -xe
+set -e
 
 for DISTRO in xenial centos7; do
     for DOCKER in distro upstream; do
-    echo Creating VMs for combo $DISTRO + $DOCKER docker
-    ./create.sh $DISTRO $DOCKER
+        log="$DISTRO-$DOCKER-`date +%s`.log"
+        echo Creating VMs for combo $DISTRO + $DOCKER docker
+        ./create.sh $DISTRO $DOCKER > $log 2>&1
 
-    echo Waiting for them to boot
-    sleep 45
+        echo Waiting for them to boot
+        sleep 45
 
-    echo Starting tests
-    log="$DISTRO-$DOCKER-`date +%s`.log"
-    if ./test.sh $DISTRO $DOCKER > $log 2>&1; then
-        echo $DISTRO $DOCKER PASS
-    else
-        echo $DISTRO $DOCKER FAIL, see $log
-    fi
+        echo Starting tests
+        if ./test.sh $DISTRO $DOCKER > $log 2>&1; then
+            echo $DISTRO $DOCKER PASS
+        else
+            echo $DISTRO $DOCKER FAIL, see $log
+        fi
 
-    echo Destroying everything
-    ./destroy-all.sh
+        echo Destroying everything
+        ./destroy-all.sh > $log 2>&1
     done
 done
