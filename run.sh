ed7c03071667049ee45c294156ddfc0fc3d3c820
Luke Marsden <me@lukemarsden.net>
2016-09-22 12:58:44 +0100

    nicer formatting again

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ set -e
 for DISTRO in xenial centos7; do
     for DOCKER in distro upstream; do
         log="$DISTRO-$DOCKER-`date +%s`.log"
-        echo "--> distro $DISTRO + $DOCKER docker, $log"
+        echo "--> distro: $DISTRO, docker: $DOCKER docker, log: $log"
         ./create.sh $DISTRO $DOCKER >> $log 2>&1
 
         echo Waiting for them to boot >> $log
@@ -12,9 +12,9 @@ for DISTRO in xenial centos7; do
 
         echo Starting tests >> $log
         if ./test.sh $DISTRO $DOCKER >> $log 2>&1; then
-            echo distro: $DISTRO, docker: $DOCKER, result: PASS
+            echo result: PASS
         else
-            echo distro: $DISTRO, docker: $DOCKER, result: FAIL
+            echo result: FAIL
         fi
 
         echo Attempting to destroy everything >> $log
