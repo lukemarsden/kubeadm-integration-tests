9d877a51e518dcf09e2da4cb0d7ede3f282e48e4
Luke Marsden <me@lukemarsden.net>
2016-09-22 10:57:42 +0100

    use parallel

--- test.sh
+++ test.sh
@@ -3,18 +3,14 @@ set -xe
 DISTRO=${1:-ubuntu}
 # Install the master
 if [ "$DISTRO" = "ubuntu" ]; then
-    # common stuff
-    for X in {1..3}; do
-        # curl -sSL https://get.docker.com/ | sh && \
-        # ubuntu package variant below v  docker official images variant above ^
-        tugboat ssh kubeadm-$DISTRO-$X -c "apt-get install -y docker.io && \
+    # curl -sSL https://get.docker.com/ | sh && \
+    # ubuntu package variant below v  docker official images variant above ^
+    common_setup="apt-get install -y docker.io && \
             apt-get install -y socat && \
             curl -s -L 'https://www.dropbox.com/s/tso6dc7b94ch2sk/debs-5ab576.txz?dl=1' | tar xJv && \
             dpkg -i debian/bin/unstable/xenial/*.deb"
-    done
 elif [ "$DISTRO" = "centos" ]; then
-    for X in {1..3}; do
-        tugboat ssh kubeadm-$DISTRO-$X -c "cat <<EOF > /etc/yum.repos.d/k8s.repo
+    common_setup="cat <<EOF > /etc/yum.repos.d/k8s.repo
 [kubelet]
 name=kubelet
 baseurl=http://files.rm-rf.ca/rpms/kubelet/
@@ -25,12 +21,14 @@ setenforce 0
 yum install -y kubelet kubeadm kubectl kubernetes-cni docker
 systemctl enable docker && systemctl start docker
 systemctl enable kubelet && systemctl start kubelet"
-    done
 fi
+
+# common setup
+parallel -i tugboat ssh kubeadm-$DISTRO-{} -c "$common_setup" -- {1..3}
 
 # install the master
 tugboat ssh kubeadm-$DISTRO-$X -c "kubeadm init |tee init-output.txt"
-join_cmd=`tugboat ssh kubeadm-$DISTRO-$X -c "tail -n 1 init-output.txt"`
+join_cmd=`tugboat ssh kubeadm-$DISTRO-$X -c "tail -n 1 init-output.txt" |tail -n 1`
 echo "GOT JOIN COMMAND $join_cmd"
 # run the command the master gave us on the nodes
 for X in {2..3}; do
