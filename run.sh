9f1b9354bafdab9810478539145dc12586304096
Luke Marsden <me@lukemarsden.net>
2016-09-22 12:10:37 +0100

    better logs

--- run.sh
+++ run.sh
@@ -5,13 +5,13 @@ for DISTRO in xenial centos7; do
     for DOCKER in distro upstream; do
         log="$DISTRO-$DOCKER-`date +%s`.log"
         echo Creating VMs for combo $DISTRO + $DOCKER docker
-        ./create.sh $DISTRO $DOCKER > $log 2>&1
+        ./create.sh $DISTRO $DOCKER >> $log 2>&1
 
         echo Waiting for them to boot
         sleep 45
 
         echo Starting tests
-        if ./test.sh $DISTRO $DOCKER > $log 2>&1; then
+        if ./test.sh $DISTRO $DOCKER >> $log 2>&1; then
             echo $DISTRO $DOCKER PASS
         else
             echo $DISTRO $DOCKER FAIL, see $log
