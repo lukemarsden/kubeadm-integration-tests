cb0b4d2991343378471bafd548251629b18b74b4
Luke Marsden <me@lukemarsden.net>
2016-09-23 16:08:36 +0200

    fixup

--- destroy-all.sh
+++ destroy-all.sh
@@ -2,6 +2,8 @@
 set -x
 for DISTRO in xenial centos7; do
     for DOCKER in distro upstream; do
-        for X in {1..3}; do yes |tugboat destroy kubeadm-$DISTRO-$DOCKER-$X; done
+        for MULTINODE in 0 1 ; do
+            for X in {1..3}; do yes |tugboat destroy kubeadm-$DISTRO-$DOCKER-$MULTINODE-$X; done
+        done
     done
 done
